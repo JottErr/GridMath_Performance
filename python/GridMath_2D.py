423e8a6fd0ef9d29c95549cd495eb276cdcdb39e
Jarik R <jarik.ruedebusch@web.de>
2023-08-24 22:37:33 +0200

    Add new method: add_grid_at_pos() in NestedGrid
    This commit extends the functionality of the NestedGrid class by
    enabling the combination of multiple grids and their values in a
    flexible manner.

--- python/GridMath_2D.py
+++ python/GridMath_2D.py
@@ -46,7 +46,7 @@ class NestedGrid():
 		"""
 		self.grid = [[0.0 for _ in range(self.height)] for _ in range(self.width)]
 
-	def radiate_value_around_position(self, position, radius, curve, magnitude=1.0):
+	def radiate_value_around_position(self, position, radius, curve, magnitude = 1.0):
 		"""
 		Calculates a value for each cell in the grid that is within a specified radius around a given position.
 		A Curve is used to calculate the fall-off of the values based on the distance of the cell from the position.
@@ -67,6 +67,27 @@ class NestedGrid():
 				distance = min(1.0, distance / radius)
 				self.grid[x][y] = curve.calculate_value(distance) * magnitude
 	
+	def add_grid_at_pos(self, other_grid, position, magnitude = 1.0):
+		"""
+		Adds a another grid to this grid. The specified position describes a cell in this grid.
+		The center of the other grid is located at that position. The other grid is scaled by the magnitude. 
+		"""
+		start_x = position[0] - other_grid.center[0]
+		start_y = position[1] - other_grid.center[1]
+		end_x = position[0] + other_grid.center[0] + 1
+		end_y  = position[1] + other_grid.center[1] + 1
+        
+		min_x = max(0, start_x)
+		min_y = max(0, start_y)
+		max_x = min(end_x, self.width)
+		max_y = min(end_y, self.height)
+        
+		for x in range(min_x, max_x):
+			for y in range(min_y, max_y):
+				other_grid_coords = (x - start_x, y - start_y)
+				value = other_grid.grid[other_grid_coords[0]][other_grid_coords[1]]
+				self.grid[x][y] += value * magnitude
+
 	def __str__(self):
 		result = ""
 		for y in range(self.height):
