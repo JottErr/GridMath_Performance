7e180f660d659cb3382406fa3ddffd5b81c0578b
Jarik R <jarik.ruedebusch@web.de>
2023-08-27 13:08:27 +0200

    Add new classes to Godot performance test

--- godot/Main.gd
+++ godot/Main.gd
@@ -1,14 +1,20 @@
 extends Node
 
 
-
 func _ready() -> void:
 	var testcurve := mCurve.new(-1.0, 1.0, 0.0, 1.0)
+	
+	
 	test_performance(NestedGrid.new(9, 9), NestedGrid.new(600, 600), testcurve)
+	test_performance(FlatGrid.new(9, 9), FlatGrid.new(600, 600), testcurve)
+	test_performance(DictGrid.new(9, 9), DictGrid.new(600, 600), testcurve)
+	
 	test_performance(NestedGrid.new(75, 75), NestedGrid.new(75, 75), testcurve)
+	test_performance(FlatGrid.new(75, 75), FlatGrid.new(75, 75), testcurve)
+	test_performance(DictGrid.new(75, 75), DictGrid.new(75, 75), testcurve)
 
 
-func test_performance(template: NestedGrid, grid: NestedGrid, curve: mCurve) -> void:
+func test_performance(template, grid, curve: mCurve) -> void:
 	template.radiate_value_around_position(template.center, template.center.x, curve)
 	var start := Time.get_ticks_usec()
 	for i in range(grid.width):
