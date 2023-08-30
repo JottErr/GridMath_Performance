80788c028052cc91af7c45989929b62312d4148d
Jarik R <jarik.ruedebusch@web.de>
2023-08-30 13:32:47 +0200

    Add NestedGrid to C++ performance test
    The performance_test has been duplicated to account for both NestedGrid
    and FlatGrid. This could be solved by inheritance - introducing a Grid
    class with virtual functions that derived classes like FlatGrid
    override. Another solution could be using templates.

--- cpp/PerformanceTest.cpp
+++ cpp/PerformanceTest.cpp
@@ -1,9 +1,28 @@
 #include "Curve.h"
 #include "FlatGrid.h"
+#include "NestedGrid.h"
 #include <chrono>
 #include <iostream>
 
-void performance_test(FlatGrid &template_grid, FlatGrid &grid, const Curve &curve) {
+void performance_test_nested(int size_templ, int size_grid, const Curve &curve) {
+	NestedGrid template_grid = NestedGrid(size_templ, size_templ);
+	NestedGrid grid = NestedGrid(size_grid, size_grid);
+	template_grid.radiate_value_around_position(template_grid.get_center(), template_grid.get_center().x, curve, 1.0f);
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < grid.get_width(); i++) {
+		for (int j = 0; j < grid.get_height(); j++) {
+			grid.add_grid_at_pos(template_grid, Vector2i(i, j));
+			//template_grid.add_into_map_at_pos(testboard, position);
+		}
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+	std::cout << "Laufzeit: " << duration << " Mikrosekunden" << std::endl;
+}
+
+void performance_test_flat(int size_templ, int size_grid, const Curve& curve) {
+	FlatGrid template_grid = FlatGrid(size_templ, size_templ);
+	FlatGrid grid = FlatGrid(size_grid, size_grid);
 	template_grid.radiate_value_around_position(template_grid.get_center(), template_grid.get_center().x, curve, 1.0f);
 	auto start = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < grid.get_width(); i++) {
@@ -19,9 +38,14 @@ void performance_test(FlatGrid &template_grid, FlatGrid &grid, const Curve &curv
 
 int main() {
 	const Curve testcurve = { -1.0f, 1.0f, 0.0f, 1.0f };
-	FlatGrid t = FlatGrid(75, 75);
-	FlatGrid g = FlatGrid(75, 75);
-	performance_test(t, g, testcurve);
+	performance_test_nested(9, 600, testcurve);
+	performance_test_flat(9, 600, testcurve);
+	performance_test_nested(9, 600, testcurve);
+	performance_test_flat(9, 600, testcurve);
+	performance_test_nested(9, 600, testcurve);
+	performance_test_flat(9, 600, testcurve);
+	performance_test_nested(9, 600, testcurve);
+	performance_test_flat(9, 600, testcurve);
 	char waiter;
 	std::cin >> waiter;
 
