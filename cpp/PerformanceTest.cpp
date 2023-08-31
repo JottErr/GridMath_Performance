169981a484af7dab570d7c2d243165bbfa3ecc8f
Jarik R <jarik.ruedebusch@web.de>
2023-08-31 11:46:55 +0200

    Updated the performance_test
    Updated the performance_test function to handle both FlatGrid and
    NestedGrid classes using templates. The code duplication for the two
    classes has been eliminated, resulting in cleaner and more maintainable
    code. While inheritance and virtual functions were considered, the focus
    remains on finding a single optimal grid class, making templates a
    faster and equally effective solution for this context.

--- cpp/PerformanceTest.cpp
+++ cpp/PerformanceTest.cpp
@@ -4,31 +4,15 @@
 #include <chrono>
 #include <iostream>
 
-void performance_test_nested(int size_templ, int size_grid, const Curve &curve) {
-	NestedGrid template_grid = NestedGrid(size_templ, size_templ);
-	NestedGrid grid = NestedGrid(size_grid, size_grid);
+template <typename GridType>
+void performance_test(int size_templ, int size_grid, const Curve &curve) {
+	GridType template_grid(size_templ, size_templ);
+	GridType grid(size_grid, size_grid);
 	template_grid.radiate_value_around_position(template_grid.get_center(), template_grid.get_center().x, curve, 1.0f);
 	auto start = std::chrono::high_resolution_clock::now();
 	for (int i = 0; i < grid.get_width(); i++) {
 		for (int j = 0; j < grid.get_height(); j++) {
 			grid.add_grid_at_pos(template_grid, Vector2i(i, j));
-			//template_grid.add_into_map_at_pos(testboard, position);
-		}
-	}
-	auto end = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-	std::cout << "Laufzeit: " << duration << " Mikrosekunden" << std::endl;
-}
-
-void performance_test_flat(int size_templ, int size_grid, const Curve& curve) {
-	FlatGrid template_grid = FlatGrid(size_templ, size_templ);
-	FlatGrid grid = FlatGrid(size_grid, size_grid);
-	template_grid.radiate_value_around_position(template_grid.get_center(), template_grid.get_center().x, curve, 1.0f);
-	auto start = std::chrono::high_resolution_clock::now();
-	for (int i = 0; i < grid.get_width(); i++) {
-		for (int j = 0; j < grid.get_height(); j++) {
-			grid.add_grid_at_pos(template_grid, Vector2i(i, j));
-			//template_grid.add_into_map_at_pos(testboard, position);
 		}
 	}
 	auto end = std::chrono::high_resolution_clock::now();
@@ -38,14 +22,9 @@ void performance_test_flat(int size_templ, int size_grid, const Curve& curve) {
 
 int main() {
 	const Curve testcurve = { -1.0f, 1.0f, 0.0f, 1.0f };
-	performance_test_nested(9, 600, testcurve);
-	performance_test_flat(9, 600, testcurve);
-	performance_test_nested(9, 600, testcurve);
-	performance_test_flat(9, 600, testcurve);
-	performance_test_nested(9, 600, testcurve);
-	performance_test_flat(9, 600, testcurve);
-	performance_test_nested(9, 600, testcurve);
-	performance_test_flat(9, 600, testcurve);
+	performance_test<NestedGrid>(9, 600, testcurve);
+	performance_test<FlatGrid>(9, 600, testcurve);
+
 	char waiter;
 	std::cin >> waiter;
 
