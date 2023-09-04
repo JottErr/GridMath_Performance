fe0f29f7b349e9868242d379676ef56aaece2835
Jarik R <jarik.ruedebusch@web.de>
2023-09-04 11:51:02 +0200

    Add new class DictGrid
    Introduces a new class DictGrid to Godot. Just as the FlatDictGrid it
    represents a two-dimensional grid structure with a dictionary. Instead
    of using Integers as keys, DictGrid maps cell coordinates with a
    Vector2i to their values. This new class allows to compare the
    performance between creation of a Vector2i and a calculation to access
    values in a dictionary.

--- godot/Main.gd
+++ godot/Main.gd
@@ -12,6 +12,9 @@ func _ready() -> void:
 	print("FlatDictGrid")
 	for i in range(30):
 		print(test_performance(FlatDictGrid.new(9, 9), FlatDictGrid.new(600, 600), testcurve))
+	print("DictGrid")
+	for i in range(30):
+		print(test_performance(DictGrid.new(9, 9), DictGrid.new(600, 600), testcurve))
 
 
 func test_performance(template, grid, curve: mCurve) -> String:
